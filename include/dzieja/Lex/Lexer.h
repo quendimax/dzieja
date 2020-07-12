fa8a143526cc9ecdde4293b0268dd96e9f9e30a1
Maksim Tomkowicz <quendimax@gmail.com>
2020-07-12 23:37:02 +0300

    Add Lex library
    - Add Lex library
    - Add `.clang-format` file
    - Add template of the Lexer class

---
+++ include/dzieja/Lex/Lexer.h
@@ -0,0 +1,36 @@
+//
+// Created by quendimax on 12.07.20.
+//
+
+#ifndef DZIEJA_LEX_LEXER_H
+#define DZIEJA_LEX_LEXER_H
+
+namespace llvm {
+
+class MemoryBuffer;
+
+} // namespace llvm
+
+namespace dzieja {
+
+class Token;
+
+class Lexer
+{
+    const char *BufferStart;
+    const char *BufferEnd;
+    const char *BufferPtr;
+
+public:
+    Lexer(const char *BufStart, const char *BufPtr, const char *BufEnd);
+    explicit Lexer(const llvm::MemoryBuffer *InputFile);
+
+    Lexer(const Lexer &) = delete;
+    Lexer &operator=(const Lexer &) = delete;
+
+    void Lex(Token &Result);
+};
+
+} // namespace dzieja
+
+#endif // DZIEJA_LEX_LEXER_H
---
+++ lib/Lex/Lexer.cpp
@@ -0,0 +1,36 @@
+#include "dzieja/Lex/Lexer.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/ADT/StringSwitch.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include <cassert>
+
+using namespace llvm;
+
+namespace dzieja {
+
+Lexer::Lexer(const char *BufStart, const char *BufPtr, const char *BufEnd)
+    : BufferStart(BufStart), BufferEnd(BufEnd), BufferPtr(BufPtr)
+{
+    assert(BufEnd[0] == '\0' && "expected null at the end of the buffer");
+
+    // Check whether we have a BOM in the beginning of the buffer
+    if (BufferStart == BufferPtr) {
+        StringRef Buffer(BufferStart, BufferEnd - BufferStart);
+        size_t BOMLength = StringSwitch<size_t>(Buffer)
+            .StartsWith("xEFxBBxBF", 3)
+            .Default(0);
+        BufferStart += BOMLength;
+    }
+}
+
+Lexer::Lexer(const MemoryBuffer *InputFile)
+    : Lexer(InputFile->getBufferStart(), InputFile->getBufferStart(), InputFile->getBufferEnd())
+{
+}
+
+void Lexer::Lex(Token &Result)
+{
+
+}
+
+} // namespace dzieja
